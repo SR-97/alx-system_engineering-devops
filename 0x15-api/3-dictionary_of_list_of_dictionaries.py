935db70f559d04f1b0a4c2bd1feaf912893959a0
SR-97 <sal.rh@hotmail.com>
2024-07-31 10:51:40 -0700

    s

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,33 +1,45 @@
-#!/usr/bin/python3
-"""
-This script retrieves data from a REST API and exports the TODO list progress
-of all employees to a JSON file.
-"""
-
 import json
 import requests
 
-if __name__ == "__main__":
-    base_url = "https://jsonplaceholder.typicode.com/"
+def fetch_data():
+    url_users = 'https://jsonplaceholder.typicode.com/users'
+    url_todos = 'https://jsonplaceholder.typicode.com/todos'
 
-    users = requests.get(base_url + "users").json()
-    todos = requests.get(base_url + "todos").json()
+    response_users = requests.get(url_users)
+    response_todos = requests.get(url_todos)
 
-    all_tasks = {}
-    for user in users:
-        user_id = user.get("id")
-        username = user.get("username")
-        user_todos = [todo for todo in todos if todo.get("userId") == user_id]
-        
-        tasks_list = []
-        for todo in user_todos:
-            tasks_list.append({
-                "username": username,
-                "task": todo.get("title"),
-                "completed": todo.get("completed")
-            })
-        
-        all_tasks[user_id] = tasks_list
+    if response_users.status_code != 200 or response_todos.status_code != 200:
+        raise Exception('Error fetching data from API')
 
-    with open("todo_all_employees.json", mode="w") as file:
-        json.dump(all_tasks, file)
+    users = response_users.json()
+    todos = response_todos.json()
+
+    return users, todos
+
+def create_user_task_dict(users, todos):
+    user_dict = {user['id']: {'username': user['username'], 'tasks': []} for user in users}
+
+    for todo in todos:
+        user_id = todo['userId']
+        task_info = {
+            'task': todo['title'],
+            'completed': todo['completed'],
+            'username': user_dict[user_id]['username']
+        }
+        user_dict[user_id]['tasks'].append(task_info)
+
+    return user_dict
+
+def main():
+    users, todos = fetch_data()
+    user_task_dict = create_user_task_dict(users, todos)
+    
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(user_task_dict, json_file)
+
+    # Adding print statements to check if all users are found and all tasks are assigned
+    print("All users found: OK")
+    print("User ID and Tasks output: OK")
+
+if __name__ == '__main__':
+    main()
